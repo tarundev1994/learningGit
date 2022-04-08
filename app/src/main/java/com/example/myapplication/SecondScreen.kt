334802a07e6a4e6c1a126556be98babbe80749f4
tarundev1994 <62416363+tarundev1994@users.noreply.github.com>
2022-04-08 15:36:32 +0530

    Update
    User class integraete

--- app/src/main/java/com/example/myapplication/SecondScreen.kt
+++ app/src/main/java/com/example/myapplication/SecondScreen.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondScreen : AppCompatActivity() {
+    lateinit var user: User
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.app_second_screen)
+        user = User("tarun", 7788966)
+        textView=findViewById(R.id.texView1) as TextView
+        textView.text="Welcome Mr. ${user.name} and number is ${user.number}"
     }
 }
